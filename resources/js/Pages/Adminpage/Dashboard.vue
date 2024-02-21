0f1c3bb685b27a8157d641accee8658a8e3b244f
Rovines <rovineshutabarat23@gmail.com>
2024-02-21 17:57:54 +0700

    First Commit - Fresh Project Setup

---
+++ resources/js/Pages/Adminpage/Dashboard.vue
@@ -0,0 +1,7 @@
+<script>
+export default {};
+</script>
+
+<template>
+    <div>iavbubdw</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+        },
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    ...require("daisyui/src/theming/themes")["light"],
+                    primary: "#F3F5F6",
+                    secondary: "#20C71E",
+                    accent: "#C8ECCA",
+                    neutral: "#050505",
+                    "base-100": "#F8FAFB",
+                },
+                dark: {
+                    ...require("daisyui/src/theming/themes")["dark"],
+                    primary: "#151515",
+                    secondary: "#1BA019",
+                    accent: "#151C15",
+                    neutral: "#050505",
+                    "base-100": "#050505",
+                },
+            },
+        ],
+    },
+};
